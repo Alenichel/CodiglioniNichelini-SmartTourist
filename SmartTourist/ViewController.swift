a74080ca9fba7693c4f9e1a6355c8c268db9f262
Fabio Codiglioni <fabiocody@icloud.com>
2019-11-23 14:57:16 +0100

    Add XCode project

---
+++ SmartTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartTourist
+//
+//  Created by Fabio Codiglioni on 23/11/2019.
+//  Copyright © 2019 Fabio Codiglioni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
