8116a52d8da934f75b971afd0fc43308a939f6f7
Fabio Codiglioni <fabiocody@icloud.com>
2019-11-27 12:06:25 +0100

    Track Dependencies folder

---
+++ SmartTourist/Dependencies/DependenciesContainer.swift
@@ -0,0 +1,38 @@
+//
+//  DependenciesContainer.swift
+//  SmartTourist
+//
+//  Created 23/11/2019.
+//
+
+import Foundation
+import Katana
+import Tempura
+import GooglePlaces
+
+
+final class DependenciesContainer: NavigationProvider {
+    let promisableDispatch: PromisableStoreDispatch
+    let getAppState: () -> AppState
+    let navigator = Navigator()
+    let placesAPI = GooglePlacesAPI()
+    
+    var getState: () -> State {
+        return self.getAppState
+    }
+
+    init(dispatch: @escaping PromisableStoreDispatch, getAppState: @escaping () -> AppState) {
+        self.promisableDispatch = dispatch
+        self.getAppState = getAppState
+    }
+    
+    convenience init(dispatch: @escaping PromisableStoreDispatch, getState: @escaping GetState) {
+        let getAppState: () -> AppState = {
+            guard let state = getState() as? AppState else {
+                fatalError("Wrong State Type")
+            }
+            return state
+        }
+        self.init(dispatch: dispatch, getAppState: getAppState)
+    }
+}
---
+++ SmartTourist/Dependencies/GooglePlacesAPI.swift
@@ -0,0 +1,51 @@
+//
+//  GooglePlacesAPI.swift
+//  SmartTourist
+//
+//  Created on 24/11/2019.
+//
+
+import Foundation
+import GoogleMaps
+import GooglePlaces
+import Hydra
+
+
+class GooglePlacesAPI {
+    
+    var placesClient = GMSPlacesClient.shared()
+    
+    enum PlaceType: String {
+        case touristAttraction = "tourist_attraction"
+    }
+    
+    func getNearbyPlaces() -> Promise<[GMSPlace]> {
+        return Promise<[GMSPlace]>(in: .main) { resolve, reject, status in
+            self.placesClient.currentPlace { (placeLikelihoodList, error) -> Void in
+                if let error = error {
+                    print(error.localizedDescription)
+                    reject(error)
+                }
+                if let placeLikelihoodList = placeLikelihoodList {
+                    let places = placeLikelihoodList.likelihoods
+                        .map({$0.place})
+                    print("Nearby places: \(places.map({$0.name ?? ""}))")
+                    resolve(places)
+                }
+            }
+        }
+    }
+    
+    func getNearbyAttractions() -> Promise<[GMSPlace]> {
+        return Promise<[GMSPlace]>(in: .background) { resolve, reject, status in
+            self.getNearbyPlaces().then { places in
+                let attractions = places.filter({$0.types?.contains(PlaceType.touristAttraction.rawValue) ?? false})
+                print("Nearby attractions: \(attractions.map({$0.name ?? ""}))")
+                resolve(attractions)
+            }.catch { error in
+                reject(error)
+            }
+        }
+    }
+    
+}
