93a78f6d027151274300d06517209afb67d1a4aa
Alessandro Nichelini <ale.nichelg@gmail.com>
2019-11-27 18:02:59 +0100

    added style to maps

--- SmartTourist/UI/TestView/PlacesTestView.swift
+++ SmartTourist/UI/TestView/PlacesTestView.swift
@@ -42,6 +42,18 @@ class PlacesTestView: UIView, ViewControllerModellableView {
         self.addSubview(self.button)
         self.addSubview(self.activityIndicator)
         self.addSubview(self.mapView)
+        
+        do {
+             // Set the map style by passing the URL of the local file.
+             if let styleURL = Bundle.main.url(forResource: "mapStyle", withExtension: "json") {
+               mapView.mapStyle = try GMSMapStyle(contentsOfFileURL: styleURL)
+             } else {
+               NSLog("Unable to find style.json")
+             }
+           } catch {
+             NSLog("One or more of the map styles failed to load. \(error)")
+           }
+        
     }
     
     func style() {
