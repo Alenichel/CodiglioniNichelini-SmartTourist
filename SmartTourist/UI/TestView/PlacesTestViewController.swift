7fbc4dfdf7ad14d73df768c4846c2d1f1ff2f982
Fabio Codiglioni <fabiocody@icloud.com>
2019-11-27 15:21:53 +0100

    Add debug prints on location

--- SmartTourist/UI/TestView/PlacesTestViewController.swift
+++ SmartTourist/UI/TestView/PlacesTestViewController.swift
@@ -17,19 +17,27 @@ class PlacesTestViewController: ViewController<PlacesTestView>, CLLocationManage
     override func viewDidLoad() {
         super.viewDidLoad()
         if CLLocationManager.locationServicesEnabled() {
+            let authorizationStatus = CLLocationManager.authorizationStatus()
+            print(authorizationStatus.rawValue)
+            if !(authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse) {
+                print("Requesting location")
+                locationManager.requestAlwaysAuthorization()
+            } else {
+                print("Location service enabled")
+            }
             locationManager.delegate = self
             locationManager.desiredAccuracy = kCLLocationAccuracyBest
-            locationManager.requestAlwaysAuthorization()
             locationManager.startUpdatingLocation()
+            print("Starting updating location")
             locationManager.distanceFilter = 250
         } else {
             let alert = UIAlertController(title: "Attention!", message: "Location services not enabled", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
             self.present(alert, animated: true)
         }
-        if self.state.firstLaunch {
+        /*if self.state.firstLaunch {
             self.dispatch(Show(ScreenID.welcome, animated: true))
-        }
+        }*/
     }
     
     override func setupInteraction() {
