a8524181e21cb67473ff091db33382a938db7ac1
Fabio Codiglioni <fabiocody@icloud.com>
2019-12-05 17:15:18 +0100

    Only AppState is Codable now

--- SmartTourist/State/AppState.swift
+++ SmartTourist/State/AppState.swift
@@ -14,10 +14,14 @@ import GooglePlaces
 struct AppState: State, Codable {
     var firstLaunch: Bool = true
     var locationState = LocationState()
+    
+    enum CodingKeys: CodingKey {    // Filter what properties to persist. I don't think it's necessary to persist locationState
+        case firstLaunch
+    }
 }
 
 
-struct LocationState: Codable {
+struct LocationState {
     var lastUpdate: Date = Date().advanced(by: TimeInterval(-60))
     var currentLocation: CLLocationCoordinate2D?
     var nearestPlaces: [GMSPlace] = [GMSPlace]()
