e34bc2f098d46a042237d34ad5aae95d6a890f15
Fabio Codiglioni <fabiocody@icloud.com>
2020-02-14 17:24:29 +0100

    Remove hard-coded constants

---
+++ SmartTourist/State/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  SmartTourist
+//
+//  Created on 14/02/2020
+//
+
+import Foundation
+
+
+let timeBeforeNewApiCall: Double = 30   // seconds
+let notificationTriggeringDistance = 250    // meters
+
