726af5a435372b62ac1853ac08b84a477ea29227
Alessandro Nichelini <ale.nichelg@gmail.com>
2020-02-19 19:05:11 +0100

    Using constant for circles radius

--- SmartTourist/State/Constants.swift
+++ SmartTourist/State/Constants.swift
@@ -10,4 +10,6 @@ import Foundation
 
 let apiThrottleTime: Double = 30   // seconds
 let notificationTriggeringDistance = 250    // meters
+let littleCircleRadius: Double = 200    // meters
+let bigCircleRadius: Double = 800   //meters
 
