b67c25c7a37d40a12724ad6a8720c4a622015d07
Fabio Codiglioni <fabiocody@icloud.com>
2020-03-04 19:30:40 +0100

    Minor changes

--- SmartTourist/Application/AppDelegate.swift
+++ SmartTourist/Application/AppDelegate.swift
@@ -63,8 +63,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
                 let placeID = userInfo["PLACE_ID"] as? String {
                 let store = attractionsViewController.store
                 if let place = store.state.locationState.popularPlaces.first(where: {$0.placeID == placeID}) {
-                    store.dispatch(Hide(animated: true, atomic: true)).then {
-                        store.dispatch(Show(Screen.detail, animated: true, context: place))
+                    store.dispatch(Hide(Screen.cityDetail.rawValue, animated: true, atomic: true)).then {
+                        store.dispatch(Hide(Screen.citySearch.rawValue, animated: true, atomic: true)).then {   // This doesn't actually work
+                            store.dispatch(Hide(Screen.detail.rawValue, animated: true, atomic: true)).then {
+                                store.dispatch(Show(Screen.detail, animated: true, context: place))
+                            }
+                        }
                     }
                 }
             }
