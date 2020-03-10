1ed9e5de1ffd2ba9c842e4967194d7877ce887f0
Alessandro Nichelini <ale.nichelg@gmail.com>
2020-03-10 17:36:31 +0100

    Minor changes

--- SmartTourist/State/Constants.swift
+++ SmartTourist/State/Constants.swift
@@ -10,9 +10,11 @@ import Foundation
 
 let apiThrottleTime: Double = 30   // seconds
 let notificationTriggeringDistance = 250    // meters
-let littleCircleRadius: Double = 200    // meters
-let bigCircleRadius: Double = 800   //meters
-
+let averageWalkingSpeed: Double = 1.34 //meters/second
+let littleCircleTimeRadius: Double = 5*60 //seconds
+let bigCircleTimeRadius: Double = 15*60 //seconds
+let littleCircleRadius: Double = averageWalkingSpeed*littleCircleTimeRadius // meters
+let bigCircleRadius: Double = averageWalkingSpeed*bigCircleTimeRadius//meters
 
 enum SelectedPlaceList: Int {
     case nearest = 0
