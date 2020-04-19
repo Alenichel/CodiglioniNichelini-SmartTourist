6420dbdcf40b93ac137c66442f45aed7c9e2bc97
Alessandro Nichelini <ale.nichelg@gmail.com>
2020-04-19 16:04:59 +0200

    removed distance from wiki query

--- SmartTourist/Dependencies/WikipediaAPI.swift
+++ SmartTourist/Dependencies/WikipediaAPI.swift
@@ -33,33 +33,33 @@ SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
 """
 
 let nearbyPlacesQuery = """
-SELECT DISTINCT ?place ?placeLabel ?cityLabel ?location ?distance ?image ?phoneNumber ?website ?wikipediaLink ?wikimediaLink
+SELECT DISTINCT ?place ?placeLabel ?cityLabel ?location ?image ?phoneNumber ?website ?wikipediaLink ?wikimediaLink
 WHERE
 {
-SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
-SERVICE wikibase:around {
-?place wdt:P625 ?location .
-
-bd:serviceParam wikibase:center "Point(<LONGITUDE> <LATITUDE>)"^^geo:wktLiteral .
-bd:serviceParam wikibase:radius "5" .
-bd:serviceParam wikibase:distance ?distance .
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
+  SERVICE wikibase:around {
+      ?place wdt:P625 ?location .
+      
+      bd:serviceParam wikibase:center "Point(9.1919555 45.4633256)"^^geo:wktLiteral .
+      bd:serviceParam wikibase:radius "5" .
+      bd:serviceParam wikibase:distance ?distance .
+  }
+  ?place wdt:P31 ?instance .
+  {?instance wdt:P279* wd:Q27096235}
+            UNION {?instance wdt:P279* wd:Q960648} .
+  ?place wdt:P131 ?city .
+  ?city wdt:P31 wd:Q515 .
+  ?wikipediaLink schema:about ?place;
+            schema:inLanguage "en";
+            schema:isPartOf [ wikibase:wikiGroup "wikipedia" ] .
+  OPTIONAL {?wikimediaLink schema:about ?place;
+            schema:inLanguage "en";
+            schema:isPartOf <https://commons.wikimedia.org/>} .
+  OPTIONAL {?place wdt:P18 ?image } .
+  OPTIONAL {?place wdt:P1329 ?phoneNumber}.
+  OPTIONAL {?place wdt:P856 ?website} .
 }
-?place wdt:P31 ?instance .
-{?instance wdt:P279* wd:Q27096235}
-UNION {?instance wdt:P279* wd:Q960648} .
-?place wdt:P131 ?city .
-?city wdt:P31 wd:Q515 .
-?wikipediaLink schema:about ?place;
-schema:inLanguage "en";
-schema:isPartOf [ wikibase:wikiGroup "wikipedia" ] .
-OPTIONAL {?wikimediaLink schema:about ?place;
-schema:inLanguage "en";
-schema:isPartOf <https://commons.wikimedia.org/>} .
-OPTIONAL {?place wdt:P18 ?image } .
-OPTIONAL {?place wdt:P1329 ?phoneNumber}.
-OPTIONAL {?place wdt:P856 ?website} .
-}
-#GROUP BY ?place ?placeLabel ?location ?distance ?image ?wikipediaLink ?wikimediaLink
+#GROUP BY ?place ?placeLabel ?location ?image ?wikipediaLink ?wikimediaLink
 ORDER BY ASC(?distance)
 LIMIT 100
 """
