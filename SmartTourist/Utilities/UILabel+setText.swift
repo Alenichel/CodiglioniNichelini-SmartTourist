05b937d187fe2a74fc91f7924a267ffdee92c024
Fabio Codiglioni <fabiocody@icloud.com>
2020-08-26 15:24:12 +0200

    Minor change

--- SmartTourist/Utilities/UILabel+setText.swift
+++ SmartTourist/Utilities/UILabel+setText.swift
@@ -8,7 +8,7 @@
 import UIKit
 import CoreLocation
 
-let defaultDescription = "No description is available. If you are familiar with this place, consider to contribute. Together \"we can make the world a better place\"."
+let defaultDescription = "No description is available. If you are familiar with this place, please consider contributing. Together we can make the world a better place."
 
 extension UILabel {
     func setText(title: String, completion: @escaping () -> Void) {
