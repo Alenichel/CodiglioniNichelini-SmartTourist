af62b8985832cbc81fe5f5a50b32e7eddfbe0774
Fabio Codiglioni <fabiocody@icloud.com>
2020-08-31 09:51:55 +0200

    Minor change

--- SmartTouristTests/ArrayTest.swift
+++ SmartTouristTests/ArrayTest.swift
@@ -14,12 +14,14 @@ import Hydra
 class ArrayTest: XCTestCase {
     func testSortedInsert() {
         var array = [1, 3, 7, 9]
-        let x = Int.random(in: 4..<7)
-        array.sortedInsert(x)
-        XCTAssert(array.contains(x))
-        for i in array.startIndex..<array.endIndex {
-            for j in array.startIndex...i {
-                XCTAssertLessThanOrEqual(array[j], array[i])
+        for _ in 0..<10 {
+            let x = Int.random(in: 0...10)
+            array.sortedInsert(x)
+            XCTAssert(array.contains(x))
+            for i in array.startIndex..<array.endIndex {
+                for j in array.startIndex...i {
+                    XCTAssertLessThanOrEqual(array[j], array[i])
+                }
             }
         }
     }
